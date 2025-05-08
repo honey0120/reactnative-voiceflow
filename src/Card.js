6ff98b646797c048104b1a2d9940a04c8cdac89e
ShopifyHunter0206 <mysteryhunter0206@gmail.com>
2025-05-08 11:20:17 -0500

    update

--- src/Card.js
+++ src/Card.js
@@ -3,24 +3,29 @@ import { cardStyles } from "./CardStyles";
 import { CardButton } from "./CardButton";
 
 const cardButtons = (buttonsPayload, userSendAction) => {
-    // check if there are buttons
-    return buttonsPayload ? (<CardButton buttons={buttonsPayload} userSendAction={userSendAction}/>) : (null);
-}
+  // check if there are buttons
+  return buttonsPayload ? (
+    <CardButton buttons={buttonsPayload} userSendAction={userSendAction} />
+  ) : null;
+};
 
-export const Card = ({payload, userSendAction}) => {
-    return (
-        <View style={cardStyles.container}>
-            <Image style={cardStyles.image}
-                source={{
-                uri: `${payload.imageUrl}`,
-                }} alt="Image trace"
-            />
-            <View style={cardStyles.textBox}>
-                <Text style={cardStyles.textBox.titleText}>{payload.title}</Text>
-                <Text style={cardStyles.textBox.descriptionText}>{payload.description.text}</Text>
-                {cardButtons(payload.buttons, userSendAction)}
-            </View>
-        </View>
-    );
-
+export const Card = ({ payload, userSendAction }) => {
+  return (
+    <View style={cardStyles.container}>
+      <Image
+        style={cardStyles.image}
+        source={{
+          uri: `${payload.imageUrl}`,
+        }}
+        alt="Image trace"
+      />
+      <View style={cardStyles.textBox}>
+        <Text style={cardStyles.textBox.titleText}>{payload.title}</Text>
+        <Text style={cardStyles.textBox.descriptionText}>
+          {payload.description.text}
+        </Text>
+        {cardButtons(payload.buttons, userSendAction)}
+      </View>
+    </View>
+  );
 };
